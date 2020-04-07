eb20c4f8c3f336d02a5c9be81a48efcd7eb36e9d
stephensenft21 <stephensenft21@gmail.com>
2020-04-06 21:55:54 -0500

    setting up models

--- Capstone/Models/Data/ApplicationUser.cs
+++ Capstone/Models/Data/ApplicationUser.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,8 +14,10 @@ namespace Capstone.Models.Data
     // Instead, always convert an ApplicationUser to an ApplicationUserViewModel before it get sent as JSON
     public class ApplicationUser : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string StreetAddress { get; set; }
+
+
+
+
+        public virtual ICollection<Comment> Comments { get; set; }
     }
 }
